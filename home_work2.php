0def05ff0aac27cd713063a89fb5d91f1fbef619
21MK12 <mborisov2112@gmail.cm>
2023-08-31 22:06:35 +0300

    виконав дз2 з відображенням на екран імені користувача. Також сумма декількох чисел та їх середнє арифметичне значення.

--- home_work2.php
+++ home_work2.php
@@ -1,9 +1,17 @@
 <?php
+echo "What your name?" . PHP_EOL;
+$name = fgets(STDIN);
+
+echo "Hello {$name}Nice to meet you!" . PHP_EOL;
+
+
 echo"введіть перше значення" . PHP_EOL;
-$first = fgets(STDIN);
+$first_num = fgets(STDIN);
 
 echo"введіть наступне значення" . PHP_EOL;
-$second = fgets(STDIN);
+$second_num = fgets(STDIN);
 
-$result = $first + $second;
-echo $result . PHP_EOL;
+$sum = $first_num + $second_num;
+$avr = ($first_num + $second_num)/2;
+
+echo "Сумма чисел = {$sum}". PHP_EOL . "Середнє арефметичне число: {$avr}. PHP_EOL";
