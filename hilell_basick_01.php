1dafa3487049a07f402ecf0dd211f80d201de2da
21MK12 <mborisov2112@gmail.cm>
2023-09-05 16:31:45 +0300

    виконав дз з кольорами

--- hilell_basick_01.php
+++ hilell_basick_01.php
@@ -34,8 +34,61 @@
 //$result = $first + $second;
 //echo $result . PHP_EOL;
 
-$string1 = "Test1 23123";
-echo mb_strpos($string1,3) . PHP_EOL;
+//$string1 = "Test1 23123";
+//echo mb_strpos($string1,3) . PHP_EOL;
 
+//$a = 2;
+//$b = 3;
+//$y = true;
+//$x = false;
+//$d = null;
+//
+//$result = ($a && $b)
+// echo var_dump($a && $b) . PHP_EOL;
 
+//$language = "en";
+// if ($language ==='ua') {
+//     echo 'Hello UA' . PHP_EOL;
+//
+// }elseif($language === 'en'){
+//     echo "Hello Eng" . PHP_EOL;
+//
+// } else if ($language === 'esp'){
+//     echo "Hello Esp" . PHP_EOL;
+//
+// } else{
+//     echo 'Just Hello';
+// }
+
+//$language = "ua";
+//if ($language === "ua") {
+//    $massage = 'Hello' . PHP_EOL;
+//} else {
+//    $massage = "Привіт" . PHP_EOL;
+//}
+//
+//$result = trim($massage) .PHP_EOL;
+//echo $result;
+//$language = 'ua';
+//switch ($language){
+//    case 'ua':
+//        $massage ='Привіт'; break;
+//    case 'en':
+//        $massage ='Hello'; break;
+//    default:
+//        $massage ='Just hello'; break;
+//}
+//echo $massage . PHP_EOL;
+
+//$language = 'ua';
+//$massage = match ($language) {
+//    'ua' => 'Привіт',
+//    'en' => 'Hello',
+//    default => 'Just hello',
+//};
+//echo $massage . PHP_EOL;
+
+$array = [1=>'elem1',33=>'elem2', 2=>'elem3'];
+$array[] = 'elem4';
+var_dump($array) .PHP_EOL;
 ?>
